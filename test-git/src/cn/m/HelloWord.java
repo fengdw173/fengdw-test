9e820d225db0b7a1a7103c9ad5adba3dc9c8644c
fengdw173 <8827554@qq.com>
2020-12-29 15:53:41 +0800

    提交测试1234

--- test-git/src/cn/m/HelloWord.java
+++ test-git/src/cn/m/HelloWord.java
@@ -3,8 +3,7 @@ package cn.m;
 public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("nihao");
-		System.out.println("nihao");
-		System.out.println("nihao");
+
 	
 	}
 	
